f25b28f13bfb51c84fc3d5b34daf3ecbb6cbe0f6
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-19 23:35:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,4 +6,6 @@ def key_for_min_value(name_hash)
   lowest_value = nil 
   name_hash.collect do |key, value|
     if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      
 end
